ec2623510085e3f1cd3cda16df3fcf149ddac509
Santoshb22 <sb289848@gmail.com>
2024-08-23 15:17:15 +0530

    added a padding to navbar

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import LogoImage from "../../assets/logo.png";
 
 export default function Logo({onClick}) {
-  return <img onClick={onClick} src={LogoImage} alt="logo" width={67} />;
+  return <img src={LogoImage} alt="logo" width={67} />;
 }
